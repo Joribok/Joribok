7a4aafee932c602e18c54d87224e8833fc196fb0
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-12-21 22:59:50 +0900

    feat: Button props에 요소 기본 props 추가

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BUTTON_TYPE, SIZE } from '@/constants';
 import { ButtonType, Size } from '@/types';
 
@@ -15,32 +16,34 @@ const Button = ({
   size = SIZE.MEDIUM,
   isAvailable = true,
   children,
-}: ButtonProps) => {
+  ...buttonAttribute
+}: ButtonProps &
+  React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => {
   switch (size) {
     case SIZE.SMALL:
       return (
-        <S.SmallButton type={type} isAvailable={isAvailable}>
+        <S.SmallButton type={type} isAvailable={isAvailable} {...buttonAttribute}>
           {children}
         </S.SmallButton>
       );
 
     case SIZE.MEDIUM:
       return (
-        <S.MediumButton type={type} isAvailable={isAvailable}>
+        <S.MediumButton type={type} isAvailable={isAvailable} {...buttonAttribute}>
           {children}
         </S.MediumButton>
       );
 
     case SIZE.LARGE:
       return (
-        <S.LargeButton type={type} isAvailable={isAvailable}>
+        <S.LargeButton type={type} isAvailable={isAvailable} {...buttonAttribute}>
           {children}
         </S.LargeButton>
       );
 
     default:
       return (
-        <S.MediumButton type={type} isAvailable={false}>
+        <S.MediumButton type={type} isAvailable={false} {...buttonAttribute}>
           {children}
         </S.MediumButton>
       );
