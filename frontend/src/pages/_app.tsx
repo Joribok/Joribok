b607eccbddbec78c186353bf9ea868b3842b2766
JASUN LEE <liswktjs@gmail.com>
2022-12-22 15:20:29 +0900

    feat: React Query를 활용한 SSR을 위한 환경설정

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,21 +1,24 @@
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'jotai';
-import { ThemeProvider } from '@emotion/react';
 
+import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@/styles/global';
 import theme from '@/styles/theme';
 import('../mocks');
 
-export const queryClient = new QueryClient();
-
 const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Provider>
         <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
         </QueryClientProvider>
       </Provider>
     </ThemeProvider>
