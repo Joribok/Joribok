c967bc4d6bae5dd9f25abf0d5d7fd39bfac5897f
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-01-04 17:58:16 +0900

    chore: 로그인 api 추가

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,7 +1,12 @@
-import { UserData } from '@/types/user';
+import { UserData, UserLoginData } from '@/types/user';
 import { instance } from '.';
 
 export const requestSignup = async (userData: UserData) => {
   const { data } = await instance.post('/signup', userData);
+  return data;
+};
+
+export const requestLogin = async (userLoginData: UserLoginData) => {
+  const { data } = await instance.post('/login', userLoginData);
   return data;
 };
