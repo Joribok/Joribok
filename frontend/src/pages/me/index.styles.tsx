ac0c0ee854de9f4b6abbd552e479ef58adbb2b60
이아현 <lah1203@naver.com>
2023-01-18 11:38:15 +0900

    feat: 새해 목표 작성 유도 추가 및 일부 스타일 수정

--- frontend/src/pages/me/index.styles.tsx
+++ frontend/src/pages/me/index.styles.tsx
@@ -11,11 +11,16 @@ const Container = styled.section`
 const PlanContainer = styled.section`
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 1rem;
 
   h2 {
     font-size: 1.2rem;
   }
+`;
+
+const PlanWrapper = styled.div`
+  width: 100%;
 `;
 
 const PlanBox = styled.ul`
@@ -30,4 +35,4 @@ const PlanBox = styled.ul`
   font-size: 1.1rem;
 `;
 
-export { Container, PlanContainer, PlanBox };
+export { Container, PlanContainer, PlanWrapper, PlanBox };
--- frontend/src/pages/me/index.tsx
+++ frontend/src/pages/me/index.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
 
 import { getMyInformation } from '@/api/me';
 import Box from '@/components/Box';
 import LuckyBox from '@/components/LuckyBox';
+import Button from '@/components/Button';
+import { BUTTON_TYPE } from '@/constants';
 
 import * as S from './index.styles';
 
@@ -14,14 +17,27 @@ const Me = () => {
   return (
     <S.Container>
       <S.PlanContainer>
-        <h2>나는 이런 걸 새해 목표로 잡았어요 💪</h2>
-        <Box>
-          <S.PlanBox>
-            {data.plans.map(plan => (
-              <li key={plan}>{plan}</li>
-            ))}
-          </S.PlanBox>
-        </Box>
+        {data.plans.length > 0 ? (
+          <>
+            <h2>나는 이런 걸 새해 목표로 잡았어요 💪</h2>
+            <S.PlanWrapper>
+              <Box>
+                <S.PlanBox>
+                  {data.plans.map(plan => (
+                    <li key={plan}>{plan}</li>
+                  ))}
+                </S.PlanBox>
+              </Box>
+            </S.PlanWrapper>
+          </>
+        ) : (
+          <>
+            <h2>아직 새해 목표를 작성하지 않으셨네요! 작성하러 가볼까요?</h2>
+            <Link href="/write-plan">
+              <Button type={BUTTON_TYPE.BUTTON}>작성하러 가기</Button>
+            </Link>
+          </>
+        )}
       </S.PlanContainer>
       <LuckyBox
         fortune={data.fortunes.fortune}
