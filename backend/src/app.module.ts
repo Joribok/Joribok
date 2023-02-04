2f2d25f58f4a3075fc448f473d2ead3c0669ae1e
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-02-04 17:45:45 +0900

    feat: DB 연결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 import { ArticleModule } from './article/article.module';
-import { ArticleController } from './article/article.controller';
-import { ArticleService } from './article/article.service';
 import { LuckyModule } from './lucky/lucky.module';
-import { LuckyController } from './lucky/lucky.controller';
-import { LuckyService } from './lucky/lucky.service';
+import { User } from './user/entities/user.entity';
+import { Article } from './article/entities/article.entity';
+import { Lucky } from './lucky/entities/lucky.entity';
 
 @Module({
-  imports: [UserModule, ArticleModule, LuckyModule],
-  controllers: [AppController, UserController, ArticleController, LuckyController],
-  providers: [AppService, UserService, ArticleService, LuckyService],
+  imports: [
+    UserModule,
+    ArticleModule,
+    LuckyModule,
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: 3000,
+      username: '',
+      password: '',
+      database: '',
+      entities: [User, Article, Lucky],
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
