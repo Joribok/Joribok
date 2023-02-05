34fae44b84194f701d4f90ec49ef797d6875be41
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-02-05 14:41:33 +0900

    feat: 환경 변수 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -14,13 +16,17 @@ import { Lucky } from './lucky/entities/lucky.entity';
     UserModule,
     ArticleModule,
     LuckyModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
-      type: '',
-      host: '',
-      port: 3000,
-      username: '',
-      password: '',
-      database: '',
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [User, Article, Lucky],
       synchronize: false,
     }),
